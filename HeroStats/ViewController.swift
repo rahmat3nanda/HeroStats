a7552e133c986eda8892aa5a981e7451bc7908c0
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-19 16:36:30 +0700

    Initial Commit

---
+++ HeroStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroStats
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 19/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
