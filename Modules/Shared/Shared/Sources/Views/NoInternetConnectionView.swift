d82a1a61173cd272c7af7c09d08b4a4c90850148
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-19 21:28:03 +0700

    implement WarningView.swift

--- Modules/Shared/Shared/Sources/Views/NoInternetConnectionView.swift
+++ Modules/Shared/Shared/Sources/Views/NoInternetConnectionView.swift
@@ -14,52 +14,13 @@ public protocol NoInternetConnectionViewDelegate: AnyObject {
 public class NoInternetConnectionView: UIView {
     public weak var delegate: NoInternetConnectionViewDelegate?
     
-    private lazy var iconImageView: UIImageView = {
-        let view = UIImageView()
+    private lazy var mainView: WarningView = {
+        let view = WarningView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        
         view.image = UIImage(systemName: "wifi.slash")?.withRenderingMode(.alwaysTemplate)
-        view.tintColor = .black
-        view.contentMode = .scaleAspectFit
-        
-        return view
-    }()
-    
-    private lazy var messageLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
-        label.text = "Uppsss...\nTidak ada koneksi internet"
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var retryButton: UIButton = {
-        var configuration = UIButton.Configuration.filled()
-        configuration.background.backgroundColor = .white
-        configuration.background.cornerRadius = 8
-        configuration.baseForegroundColor = .black
-        configuration.background.strokeColor = .black
-        configuration.background.strokeWidth = 1
-        configuration.title = "Retry"
-        configuration.contentInsets = .init(top: 8, leading: 16, bottom: 8, trailing: 16)
-        
-        let button = UIButton(type: .custom)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.configuration = configuration
-        return button
-    }()
-    
-    private lazy var containerView: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [iconImageView, messageLabel, retryButton])
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.axis = .vertical
-        view.spacing = 16
-        view.alignment = .center
-        
-        iconImageView.anchors.height.equal(64)
-        iconImageView.anchors.width.equal(64)
-        retryButton.anchors.width.greaterThanOrEqual(150)
+        view.message = "Uppsss...\nTidak ada koneksi internet"
+        view.titleButton = "Retry"
         
         return view
     }()
@@ -79,9 +40,11 @@ private extension NoInternetConnectionView {
     func configUI() {
         backgroundColor = .white
         
-        addSubview(containerView)
-        containerView.anchors.center.align()
-        containerView.anchors.width.lessThanOrEqual(UIScreen.main.bounds.width * 0.8)
+        addSubview(mainView)
+        mainView.anchors.center.align()
+        mainView.onAction = { [weak self] in
+            self?.delegate?.didRetry()
+        }
     }
 }
 
