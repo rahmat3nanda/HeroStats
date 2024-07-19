54c1f37aae2bcf4ebe0dab6ec264329e9b5d9cb4
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-19 17:51:39 +0700

    create NetworkError.swift

---
+++ Modules/Networking/Networking/Sources/NetworkError.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkError.swift
+//  Networking
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 19/07/24.
+//
+
+import Foundation
+
+public struct NetworkErrorResponse: Codable {
+    public let error: String?
+}
+
+public enum NetworkError: Error {
+    case connectivity
+    case invalidData
+    case response(ErrorResponse)
+}
