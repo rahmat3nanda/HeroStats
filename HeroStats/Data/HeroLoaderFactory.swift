0ec654a92a1fa751a7b8bc7ea0108e0b14299b3f
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-20 19:48:34 +0700

    Create HeroLoaderFactory.swift

---
+++ HeroStats/Data/HeroLoaderFactory.swift
@@ -0,0 +1,32 @@
+//
+//  HeroLoaderFactory.swift
+//  HeroStats
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 20/07/24.
+//
+
+import Foundation
+import Shared
+import Networking
+
+class HeroLoaderFactory {
+    
+    private init() {}
+    
+    static func create() -> HeroLoader {
+        let cacheLoader = CacheHeroLoader(cache: Cache.common, key: .hero)
+        
+        let url = URL(string: APIConstants.instance.apiBaseUrl)!
+        let client = HTTPClientFactory.makeURLSessionHttpClient()
+        let remoteLoader = RemoteHeroLoader(baseUrl: url, client: client)
+        
+        let composite = HeroLoaderComposite(
+            remoteLoader: remoteLoader,
+            cacheLoader: cacheLoader,
+            cache: Cache.common,
+            key: .hero
+        )
+        
+        return composite
+    }
+}
