bf57b1f6adb9d7a957619a83ceea522a86d34cad
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-20 19:48:40 +0700

    add log

--- HeroStats/Data/HeroLoaderComposite.swift
+++ HeroStats/Data/HeroLoaderComposite.swift
@@ -25,6 +25,7 @@ class HeroLoaderComposite: HeroLoader {
     }
     
     func load(skipCache: Bool, completion: @escaping (Result) -> Void) {
+        print("skip cache", skipCache)
         if skipCache {
             loadFromRemote(completion: completion)
             return
@@ -35,8 +36,10 @@ class HeroLoaderComposite: HeroLoader {
             
             switch result {
             case .success(let success):
+                print("Cache success")
                 completion(.success(success))
             case .failure(let failure):
+                print("Cache error", failure.localizedDescription)
                 self.loadFromRemote(completion: completion)
             }
         }
@@ -50,11 +53,14 @@ private extension HeroLoaderComposite {
             
             switch result {
             case .success(let success):
+                print("Remote success")
                 if let data = try? JSONEncoder().encode(success) {
+                    print("Cache saving")
                     self.cache.save(data: data, key: self.key)
                 }
                 completion(.success(success))
             case .failure(let failure):
+                print("Remote error", failure.localizedDescription)
                 completion(.failure(failure))
             }
         }
