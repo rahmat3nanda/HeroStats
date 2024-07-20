e173f8a9de069a6414436d798f70d4f1e40870c5
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-20 19:49:06 +0700

    remove unused

--- HeroStats/SceneDelegate.swift
+++ HeroStats/SceneDelegate.swift
@@ -15,14 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     private(set) lazy var navigationController = UINavigationController()
     
-    lazy var apiBaseUrl: URL = {
-        return URL(string: APIConstants.instance.apiBaseUrl)!
-    }()
-    
-    lazy var httpClient: HTTPClient = {
-        return makeHTTPClient(baseURL: apiBaseUrl)
-    }()
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -42,16 +34,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 extension SceneDelegate {
     func makeHeroController() -> UIViewController {
         let controller = HeroController()
-        let loader = MainQueueDispatchDecorator(decoratee: makeHeroLoader())
+        let loader = MainQueueDispatchDecorator(decoratee: HeroLoaderFactory.create())
         let presenter = HeroPresenter(view: controller, loader: loader)
         controller.presenter = presenter
         
         return controller
-    }
-    
-    func makeHeroLoader() -> HeroLoader {
-        let loader = RemoteHeroLoader(baseUrl: apiBaseUrl, client: httpClient)
-        return loader
     }
 }
 
@@ -60,17 +47,5 @@ extension MainQueueDispatchDecorator: HeroLoader where T == HeroLoader {
         decoratee.load(skipCache: skipCache) { [weak self] result in
             self?.dispatch { completion(result) }
         }
-    }
-}
-
-// MARK: - HTTPClient
-extension SceneDelegate {
-    func makeHTTPClient(baseURL: URL) ->  HTTPClient {
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.timeoutIntervalForRequest = 10.0
-        configuration.timeoutIntervalForResource = 10.0
-        
-        let httpClient = URLSessionHTTPClient(session: URLSession(configuration: configuration))
-        return httpClient
     }
 }
