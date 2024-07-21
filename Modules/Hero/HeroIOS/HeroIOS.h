de2df5c40c175077b7805bc3e180bf4fd88c3b66
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-21 12:00:14 +0700

    create module hero ios

---
+++ Modules/Hero/HeroIOS/HeroIOS.h
@@ -0,0 +1,18 @@
+//
+//  HeroIOS.h
+//  HeroIOS
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 21/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeroIOS.
+FOUNDATION_EXPORT double HeroIOSVersionNumber;
+
+//! Project version string for HeroIOS.
+FOUNDATION_EXPORT const unsigned char HeroIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeroIOS/PublicHeader.h>
+
+
