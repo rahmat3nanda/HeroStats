a8281e42a901be5e51dd6b9312002bc53e6ac6be
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-21 11:53:11 +0700

    Create module hero

---
+++ Modules/Hero/Hero/Hero.h
@@ -0,0 +1,18 @@
+//
+//  Hero.h
+//  Hero
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 21/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hero.
+FOUNDATION_EXPORT double HeroVersionNumber;
+
+//! Project version string for Hero.
+FOUNDATION_EXPORT const unsigned char HeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hero/PublicHeader.h>
+
+
