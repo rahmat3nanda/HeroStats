ce7efe34e56c1c5b7f795b46c9593e11457e0a52
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-22 00:17:37 +0700

    implement HeroItemCollectionCell.swift

--- Modules/Hero/HeroIOS/Sources/Views/Collection/HeroCollectionCell.swift
+++ Modules/Hero/HeroIOS/Sources/Views/Collection/HeroCollectionCell.swift
@@ -9,7 +9,14 @@ import UIKit
 import Shared
 import Hero
 
+protocol HeroCollectionCellDelegate: AnyObject {
+    func didSelect(_ hero: HeroItem)
+}
+
 class HeroCollectionCell: UICollectionViewCell {
+    
+    weak var delegate: HeroCollectionCellDelegate?
+    
     var data: [HeroItem] = [] {
         didSet {
             collectionView.reloadData()
@@ -39,11 +46,69 @@ class HeroCollectionCell: UICollectionViewCell {
     }
 }
 
+extension HeroCollectionCell {
+    func refreshCollection() {
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+}
+
 // MARK: - Private Helper
 private extension HeroCollectionCell {
     func configUI() {
         contentView.addSubview(collectionView)
         
         collectionView.anchors.edges.pin(to: contentView.safeAreaLayoutGuide, insets: .zero)
+        configCollectionView()
+    }
+    
+    private func configCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCustomCell(HeroItemCollectionCell.self)
+    }
+}
+
+extension HeroCollectionCell: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCustomCell(with: HeroItemCollectionCell.self, indexPath: indexPath)
+        if let item = data[safe: indexPath.item] {
+            cell.setHero(name: item.localizedName, image: item.img)
+        }
+        cell.delegate = self
+        return cell
+    }
+    
+    public func numberOfSections(in collectionView: UICollectionView) -> Int { 1 }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        data.count
+    }
+}
+
+extension HeroCollectionCell: UICollectionViewDelegate {}
+
+extension HeroCollectionCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.safeAreaLayoutGuide.layoutFrame
+        let orientation = UIDevice.current.orientation
+        let width = (orientation == .portrait || orientation == .unknown) ? (size.width - 16) : (size.width - 40) / 3
+        
+        return CGSize(width: width, height: 160)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top:12, left: 8, bottom: 12, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat { 12 }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat { 8 }
+}
+
+
+extension HeroCollectionCell: HeroItemCollectionCellDelegate {
+    func didSelect(_ cell: HeroItemCollectionCell) {
+        guard let indexPath = collectionView.indexPath(for: cell), let item = data[safe: indexPath.item] else { return }
+        self.delegate?.didSelect(item)
     }
 }
