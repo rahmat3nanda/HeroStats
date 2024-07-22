79939a3ee8f459fbfe1e2238686c18e017936e92
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-22 18:48:34 +0700

    implement recommendations

--- Modules/Hero/HeroIOS/Sources/Views/HeroDetailView.swift
+++ Modules/Hero/HeroIOS/Sources/Views/HeroDetailView.swift
@@ -13,6 +13,12 @@ import Kingfisher
 
 class HeroDetailView: UIView {
     
+    private var recommendations: [HeroItem] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
     private lazy var imageView: UIImageView = {
         let view = UIImageView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -66,6 +72,18 @@ class HeroDetailView: UIView {
         return label
     }()
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        
+        return collectionView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configUI()
@@ -84,6 +102,7 @@ extension HeroDetailView {
         attackTypeLabel.text = "Attack Type: \(hero.attackType.rawValue)"
         rolesLabel.text = "Roles: \(hero.roles.map({ $0.rawValue }).joined(separator: ", "))"
         recomLabel.text = recommendations.isEmpty ? "" : "Recommendations"
+        self.recommendations = recommendations
         
         if let url = URL(string: "\(APIConstants.instance.imageBaseUrl)\(hero.img)") {
             imageView.kf.setImage(
@@ -126,6 +145,11 @@ extension HeroDetailView {
             recomLabel.anchors.trailing.equal(safeAreaLayoutGuide.anchors.trailing, constant: -12)
             recomLabel.anchors.top.equal(rolesLabel.anchors.bottom, constant: 16)
             
+            collectionView.anchors.leading.equal(safeAreaLayoutGuide.anchors.leading, constant: 12)
+            collectionView.anchors.trailing.equal(safeAreaLayoutGuide.anchors.trailing, constant: -12)
+            collectionView.anchors.top.equal(recomLabel.anchors.bottom, constant: 12)
+            collectionView.anchors.bottom.equal(safeAreaLayoutGuide.anchors.bottom, constant: 16)
+            
             return
         }
         
@@ -154,6 +178,11 @@ extension HeroDetailView {
         recomLabel.anchors.leading.equal(imageView.anchors.trailing, constant: 12)
         recomLabel.anchors.trailing.equal(safeAreaLayoutGuide.anchors.trailing, constant: -12)
         recomLabel.anchors.top.equal(rolesLabel.anchors.bottom, constant: 16)
+        
+        collectionView.anchors.leading.equal(imageView.anchors.trailing, constant: 12)
+        collectionView.anchors.trailing.equal(safeAreaLayoutGuide.anchors.trailing, constant: -12)
+        collectionView.anchors.top.equal(recomLabel.anchors.bottom, constant: 16)
+        collectionView.anchors.bottom.equal(safeAreaLayoutGuide.anchors.bottom, constant: 12)
     }
 }
 
@@ -162,8 +191,61 @@ private extension HeroDetailView {
     func configUI() {
         backgroundColor = .white
         
-        addSubviews([imageView, nameLabel, primaryAttrLabel, attackTypeLabel, rolesLabel, recomLabel])
+        addSubviews([imageView, nameLabel, primaryAttrLabel, attackTypeLabel, rolesLabel, recomLabel, collectionView])
         setupConstraints()
+        configCollectionView()
+    }
+    
+    private func configCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCustomCell(HeroItemCollectionCell.self)
+    }
+}
+
+extension HeroDetailView: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCustomCell(with: HeroItemCollectionCell.self, indexPath: indexPath)
+        if let item = recommendations[safe: indexPath.item] {
+            cell.setHero(name: item.localizedName, image: item.img)
+        }
+        cell.delegate = self
+        return cell
+    }
+    
+    public func numberOfSections(in collectionView: UICollectionView) -> Int { 1 }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        recommendations.count
+    }
+}
+
+extension HeroDetailView: UICollectionViewDelegate {}
+
+extension HeroDetailView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.safeAreaLayoutGuide.layoutFrame
+        let orientation = UIDevice.current.orientation
+        let width = (orientation == .portrait || orientation == .unknown) ? (size.width - 16) : (size.width - 40) / 3
+        
+        return CGSize(width: width, height: 160)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top:12, left: 8, bottom: 12, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat { 12 }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat { 8 }
+}
+
+
+extension HeroDetailView: HeroItemCollectionCellDelegate {
+    func didSelect(_ cell: HeroItemCollectionCell) {
+        guard let indexPath = collectionView.indexPath(for: cell),
+              let item = recommendations[safe: indexPath.item] else { return }
+        //        delegate?.didSelect(item)
     }
 }
 
